73296de94c83e0f9950cc291470bdc3d2da40e1f
Priya <Priyamadiwalar0007@gmail.com>
2022-03-16 22:49:55 +0530

    Added: Welcome to Address Book

---
+++ src/com/blz/javastreamAPI/AddressBook.java
@@ -0,0 +1,8 @@
+package com.blz.javastreamAPI;
+
+public class AddressBook {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcom to Address book program using Stream API");
+	}
+}
